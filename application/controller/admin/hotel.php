952b99763bd87547b879e2a7ffbfb6f8a4bfff05
nguyenchien2011 <dangchien58@gmail.com>
2019-04-10 08:22:00 +0700

    a

--- application/controller/admin/hotel.php
+++ application/controller/admin/hotel.php
@@ -1,8 +1,178 @@
 <?php
+if(!class_exists('ST_Hotel_Admin')){
 class ST_Hotel_Admin{
+	public static $_inst;
 	public function __construct(){
+		add_action('init', array($this,'customsb_post_type'));	
+		add_action( 'init', array($this,'custom_taxonomy_location') );
+		add_action('init', array($this, 'hotel_custom_taxonomies'));
+		add_action('add_meta_boxes',array($this,'hotel_meta_box'));
+		add_action('save_post',array($this,'hotel_info_save'));
+		add_action('manage_hotel_posts_columns',array($this,'hotel_set_columns'));
+		add_action('manage_hotel_posts_custom_column',array($this,'hotel_custom_columns'), 10,2);
+	}
 
+	public function customsb_post_type(){
+		$labels = array(
+			'name' => 'Hotel',
+			'singular_name'=> 'Hotel',
+			'add_new' => 'Add Hotels',
+			'all_items' => 'All Hotels',
+			'add_new_item' => 'Add Hotel',
+			'edit_item' => 'Edit Item',
+			'new_item' => 'New Item',
+			'view_item' => 'View Item',
+			'search_items' => 'Search Hotel',
+			'not_found' => 'No Items Found',
+			'not_found_in_trash' => 'No items found in trash',
+			'parent_item_colon' => 'Parent Item',
+
+		);
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'has_archive' => true, // cho phep luu tru bai viet
+			'publicly_queryable' => true,
+			'query_var' => true,
+			'rewrite' => true,
+			'capability_type' => 'post',
+			'hierarchical' => true,
+			'supports' => array(
+				'title',
+				'editor',
+				'excerpt',
+				'thumbnail',
+				'revisions',
+				'page-attributes'
+			),
+			'taxonomies' => array('category', 'post_tag'),
+			'menu_position' => 4,
+			'exclude_form_search' => false
+		);
+		register_post_type('hotel', $args);
+	}
+	public function custom_taxonomy_location(){
+		$labels = array(
+			'name'  => 'Location',
+			'singular_name' => 'Location',
+			'search_items' => 'Search Location',
+			'all_items' => 'All Locations',
+			'parent_item' => 'Parent Location',
+			'parent_item_colon' => 'Parent Location:',
+			'edit_item' => 'Edit Location',
+			'update_item' => 'Update Location',
+			'add_new_item' => 'Add New Location',
+			'new_item_name' => 'New Location Name',
+			'menu_name' => 'Location',
+		);
+		$args = array(
+			'labels' => $labels,
+			'show_ui' => true,
+			'hierarchical' => true,
+			'show_admin_column' => true,
+			'query_var' => true,
+			'rewrite' => array('slug'=>'location'),
+
+		);
+		register_taxonomy('location', array('hotel','room'), $args);
+	}
+	//Create metabox of hotel
+	public function hotel_custom_taxonomies(){
+		$labels = array(
+			'name'  => 'Facilities',
+			'singular_name' => 'Facilities',
+			'search_items' => 'Search Facilities',
+			'all_items' => 'All Facilities',
+			'parent_item' => 'Parent Type',
+			'parent_item_colon' => 'Parent Type:',
+			'edit_item' => 'Edit Type',
+			'update_item' => 'Update Type',
+			'add_new_item' => 'Add New Type',
+			'new_item_name' => 'New Type Name',
+			'menu_name' => 'Facilities',
+		);
+		$args = array(
+			'labels' => $labels,
+			'show_ui' => true,
+			'hierarchical' => true,
+			'show_admin_column' => true,
+			'query_var' => true,
+
+			'rewrite' => array('slug', 'facilities'),
+
+		);
+		register_taxonomy('facilities', array('hotel'), $args);
+	}
+	//create custome column
+	function hotel_set_columns($columns){
+		$newColumns = array();
+		$newColumns['title'] = 'Hotel Name';
+		$newColumns['description'] = 'Description';
+		$newColumns['city_name'] = 'City Name';
+		$newColumns['address'] = 'Address';
+		$newColumns['facilities'] ='Facilities';
+		$newColumns['date'] = 'Date';
+		$newColumns['tags'] = 'Tags';
+		$newColumns['location'] ='Location'; 
+		return $newColumns;
+	}
+	function hotel_custom_columns($column,$post_id){
+		switch ($column){
+			case 'description':
+				echo get_the_excerpt();
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+	}
+	//create metabox of hotel
+	 function hotel_meta_box(){
+		add_meta_box('hotel-info','Hotel Information',[$this,'hotel_info_output'],'hotel');
+	}
+	 function hotel_info_output($post){
+	 	wp_nonce_field('hotel_info_save','hotel_meta_box_nonce');
+	 	$city_name = get_post_meta($post->ID,'_city_name',true);
+	 	$add = get_post_meta($post->ID,'_add',true);
+	 	?>
+	 	<p>
+	 		<label for="city_name">City Name:</label><br />
+	 		<input type="text" name="city_name" id="city_name" size="30" value="<?php echo $city_name; ?>" />
+	 	</p>
+	 	<p>
+	 		<label for="address">Address:</label><br />
+	 		<input type="text" name="address" id="address" size="30" value="<?php echo $add; ?>" />
+	 	</p>
+
+	 	<?php }
+	 	function hotel_info_save($post_id){
+	 		if(!isset($_POST['hotel_meta_box_nonce'])){
+	 			return;
+	 		}
+	 		if(!wp_verify_nonce($_POST['hotel_meta_box_nonce'],'hotel_info_save')){
+	 			return;
+	 		}
+	 		if(define('DOING_AUTOSAVE') && DOING_AUTOSAVE){
+	 			return;
+	 		}
+
+	 		$city_name = sanitize_text_field($_POST['city_name']);	
+	 		update_post_meta($post_id,'_city_name',$city_name);
+	 		$add = sanitize_text_field($_POST['address']);	
+	 		update_post_meta($post_id,'_add',$add);
+
+
+	 	}
+
+	public static function inst(){
+		if(empty(self::$_inst)){
+			self::$_inst = new self();
+		}
+		return self::$_inst;
 	}
 
 
+}
+ST_Hotel_Admin::inst();
 }
