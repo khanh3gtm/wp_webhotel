65e23326fd9de1b062a2cc0aa9dfbca5d133d51b
nguyenchien2011 <dangchien58@gmail.com>
2019-04-10 08:16:47 +0700

    a

--- inc/controller/admin/room.php
+++ inc/controller/admin/room.php
@@ -5,24 +5,118 @@ class ST_Room_Admin{
 	public function __construct(){
 		add_action('init', array($this,'room_custom_post_type'));	
 		add_action('init', array($this, 'room_custom_taxonomy'));
-		add_action('add_meta_boxes',array($this, 'room_meta_box'));
+		// add_action('add_meta_boxes',array($this, 'room_meta_box'));
+		add_filter('manage_room_posts_columns',array($this, 'sunset_set_contact_columns'));
+		add_action('manage_room_posts_custom_column', array($this,'sunset_contact_custom_column'), 10, 2);
+		add_action('add_meta_boxes', array($this, 'sunset_contact_add_meta_box'));
+		add_action('save_posts', array($this, 'sunset_save_contact_email_data'), 10, 2);
+		
 
-		add_action('save_post',array($this, 'save_your_fields_meta'));
-		add_action('amenities_add_form_fields', array ( $this, 'add_category_image' ) );
+
+		add_action('amenities_add_form_fields', array ( $this, 'add_category_image' ));
+		add_action('created_amenities', array($this, 'save_category_image'), 10, 2);
+		add_action('amenities_edit_form_fields', array ( $this, 'update_category_image' ), 10, 2 );
+		add_action('edited_amenities', array ( $this, 'updated_category_image' ), 10, 2 );
+		add_action('admin_enqueue_scripts', array( $this, 'load_media' ) );
+		add_action( 'admin_footer', array ( $this, 'add_script' ) );
 
 	}
+	public function load_media(){
+		wp_enqueue_media();
+	}
 	 public function add_category_image () { ?>
-   <div class="form-field term-group">
-     <label for="category-image-id"><?php _e('Image', 'shinetheme'); ?></label>
-     <input type="hidden" id="category-image-id" name="category-image-id" class="custom_media_url" value="">
-     <div id="category-image-wrapper"></div>
-     <p>
-       <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'shinetheme' ); ?>" />
-       <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'shinetheme' ); ?>" />
-    </p>
-   </div>
+	   <div class="form-field term-group">
+	     <label for="category-image-id"><?php _e('Image', 'shinetheme'); ?></label>
+	     <input type="hidden" id="category-image-id" name="category-image-id" class="custom_media_url" value="">
+	     <div id="category-image-wrapper"></div>
+	     <p>
+	       <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'shinetheme' ); ?>" />
+	       <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'shinetheme' ); ?>" />
+	    </p>
+	   </div>
+	 <?php
+	 }
+	
+	  public function add_script() { ?>
+   <script>
+     jQuery(document).ready( function($) {
+       function ct_media_upload(button_class) {
+         var _custom_media = true,
+         _orig_send_attachment = wp.media.editor.send.attachment;
+         $('body').on('click', button_class, function(e) {
+           var button_id = '#'+$(this).attr('id');
+           var send_attachment_bkp = wp.media.editor.send.attachment;
+           var button = $(button_id);
+           _custom_media = true;
+           wp.media.editor.send.attachment = function(props, attachment){
+             if ( _custom_media ) {
+               $('#category-image-id').val(attachment.id);
+               $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+               $('#category-image-wrapper .custom_media_image').attr('src',attachment.url).css('display','block');
+             } else {
+               return _orig_send_attachment.apply( button_id, [props, attachment] );
+             }
+            }
+         wp.media.editor.open(button);
+         return false;
+       });
+     }
+     ct_media_upload('.ct_tax_media_button.button'); 
+     $('body').on('click','.ct_tax_media_remove',function(){
+       $('#category-image-id').val('');
+       $('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+     });
+     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+     $(document).ajaxComplete(function(event, xhr, settings) {
+       var queryStringArr = settings.data.split('&');
+       if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+         var xml = xhr.responseXML;
+         $response = $(xml).find('term_id').text();
+         if($response!=""){
+           // Clear the thumb image
+           $('#category-image-wrapper').html('');
+         }
+       }
+     });
+   });
+ </script>
+ <?php }
+ 	 public function save_category_image(){
+	 	if(isset($_POST['category-image-id']) && '' !== $_POST['category-image-id']){
+	 		$image = $_POST['category-image-id'];
+	 		add_term_meta($term_id, 'category-image-id', $image, true);
+	 	}
+	 }
+	  public function update_category_image ( $term, $amenities ) { ?>
+   <tr class="form-field term-group-wrap">
+     <th scope="row">
+       <label for="category-image-id"><?php _e( 'Image', 'shinetheme' ); ?></label>
+     </th>
+     <td>
+       <?php $image_id = get_term_meta ( $term -> term_id, 'category-image-id', true ); ?>
+       <input type="hidden" id="category-image-id" name="category-image-id" value="<?php echo $image_id; ?>">
+       <div id="category-image-wrapper">
+         <?php if ( $image_id ) { ?>
+           <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+         <?php } ?>
+       </div>
+       <p>
+         <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'shinetheme' ); ?>" />
+         <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'shinetheme' ); ?>" />
+       </p>
+     </td>
+   </tr>
  <?php
  }
+  public function updated_category_image ( $term_id, $tt_id ) {
+   if( isset( $_POST['category-image-id'] ) && '' !== $_POST['category-image-id'] ){
+     $image = $_POST['category-image-id'];
+     update_term_meta ( $term_id, 'category-image-id', $image );
+   } else {
+     update_term_meta ( $term_id, 'category-image-id', '' );
+   }
+ }
+
 	public function room_custom_post_type(){
 		$labels = array(
 			'name' => 'Room',
@@ -88,120 +182,158 @@ class ST_Room_Admin{
 		);
 		register_taxonomy('amenities', array('room'), $args);
 	}
-	function room_meta_box(){
-		add_meta_box('room-info', 'Thông tin về phòng', [$this,'show_your_fields_meta_box'], 'room');
+	function sunset_set_contact_columns($columns)
+{
+	$newColumns = array();
+	$newColumns['title'] = '__Full Name';
+	$newColumns['message'] = 'Message';
+	$newColumns['email'] = 'Email';
+	$newColumns['tags'] = 'Tags';
+	$newColumns['date'] = 'Date';
+	return $newColumns;
+}
+function sunset_contact_custom_column($column,$post_id)
+{
+	switch ($column) {
+		case 'message':
+			echo get_the_excerpt();
+			break;
+		
+		case 'email':
+			$email = get_post_meta($post_id,'_contact_email_value_key',true);
+			echo '<a href = "mailto:'.$email.'">'.$email.'</a>';
+			break;
 	}
-	function show_your_fields_meta_box($post){
-		global $post;
-		$meta = get_post_meta($post->ID, 'your_fields', true);?>
-		<input type="hidden" name="your_meta_box_nonce" value="<?php echo wp_create_nonce(basename(__FILE__));?>">
-		<p>
-	    	<label for="superficies">Superficies</label>
-	    	<br>
-	    	<?php $current_text = isset($meta['text']) ? $meta[text] : ''; ?>
-	    	<input type="text" name="your_fields[text_s]" id="your_fields[text_s]" class="regular-text" value="<?php echo $current_text; ?>">
-	    	<!-- <?php echo ('<input type="text" id="your_fields[text_s]" name="your_fields[text_s]" value="'.$meta.'"/>'); ?> -->
-	    	</p>
-    	<p>
-    		<label for="">Prices</label>
-    		<br>
-    		<input type="text" name="your_fields[text_p]" id="your_fields[text_p]" class="regular-text" value="<?php echo $current_text; ?>">
-	    	</p>
-    	</p>
-    	<!-- <p>
-    	<label for="your_fields[textarea]">Textarea</label>
-    	<br>
-    	<textarea name="your_fields[textarea]" id="your_fields[textarea]" rows="5" cols="30" style="width:500px;"><?php echo $meta['textarea']; ?></textarea>
-    	</p>
-    	</p> -->
-    	<!--checkbox</!-->
-    	<!-- <p>
-    	<label for="your_fields[checkbox]">Checkbox
-    		<input type="checkbox" name="your_fields[checkbox]" value="checkbox" <?php if ( $meta['checkbox'] === 'checkbox' ) echo 'checked'; ?>>
-    	</label>
-    	</p> -->
-    	<!--select menu</!-->
-    	<?php
-    		$current_option = isset($meta['select'])  ? $meta['select'] : '';
-    	?>
-		<p>
-	    	<label for="bed">Beds</label>
-	    	<br>
-	    	<select name="your_fields[bed]" id="your_fields[bed]">
-	    		<option value="option-one" ><?php selected( $current_option, 'option-one' ); ?>1</option>
-	    		<option value="option-two" ><?php selected( $current_option, 'option-two' ); ?>2</option>
-	    		<option value="option-three" ><?php selected( $current_option, 'option-three' ); ?>3</option>
-	    		<option value="option-four" ><?php selected( $current_option, 'option-four' ); ?>4</option>
-	    		<option value="option-five" ><?php selected( $current_option, 'option-five' ); ?>5</option>
-	    		<option value="option-six" ><?php selected( $current_option, 'option-six' ); ?>6</option>
-	    		<option value="option-seven" ><?php selected( $current_option, 'option-seven' ); ?>7</option>
-	    		<option value="option-eight" ><?php selected( $current_option, 'option-eight' ); ?>8</option>
+}
 
-	    	</select>
-    	</p>
 
-    	<p>
-	    	<label for="children">Children</label>
-	    	<br>
-	    	<select name="your_fields[children]" id="your_fields[children]">
-	    		<option value="option-one" ><?php selected( $current_option, 'option-one' ); ?>1</option>
-	    		<option value="option-two" ><?php selected( $current_option, 'option-two' ); ?>2</option>
-	    		<option value="option-three" ><?php selected( $current_option, 'option-three' ); ?>3</option>
-	    		<option value="option-four" ><?php selected( $current_option, 'option-four' ); ?>4</option>
-	    		<option value="option-five" ><?php selected( $current_option, 'option-five' ); ?>5</option>
-	    		<option value="option-six" ><?php selected( $current_option, 'option-six' ); ?>6</option>
-	    		<option value="option-seven" ><?php selected( $current_option, 'option-seven' ); ?>7</option>
-	    		<option value="option-eight" ><?php selected( $current_option, 'option-eight' ); ?>8</option>
 
-	    	</select>
-    	</p>
+function sunset_contact_add_meta_box()
+{
+	add_meta_box('contact_email','User Email',[$this,'sunset_contact_email_callback'],'room');
 
-    	<p>
-	    	<label for="adult">Adults</label>
-	    	<br>
-	    	<select name="your_fields[adult]" id="your_fields[adult]">
-	    		<option value="option-one" ><?php selected( $current_option, 'option-one' ); ?>1</option>
-	    		<option value="option-two" ><?php selected( $current_option, 'option-two' ); ?>2</option>
-	    		<option value="option-three" ><?php selected( $current_option, 'option-three' ); ?>3</option>
-	    		<option value="option-four" ><?php selected( $current_option, 'option-four' ); ?>4</option>
-	    		<option value="option-five" ><?php selected( $current_option, 'option-five' ); ?>5</option>
-	    		<option value="option-six" ><?php selected( $current_option, 'option-six' ); ?>6</option>
-	    		<option value="option-seven" ><?php selected( $current_option, 'option-seven' ); ?>7</option>
-	    		<option value="option-eight" ><?php selected( $current_option, 'option-eight' ); ?>8</option>
+}
+function sunset_contact_email_callback($post){
+	wp_nonce_field('sunset_save_contact_email_data', 'sunset_contact_email_meta_box_nonce');
+	$value = get_post_meta($post->ID, '_contact_email_value_key', true);
+	echo '<label for="sunset_contact_email_field">User email</label>';
+	echo '<input type="email" id="sunset_contact_email_field" name="sunset_contact_email_field" value="' . esc_attr($value) . '">';
 
-	    	</select>
-    	</p>
-	<?php }
+}
+function sunset_save_contact_email_data($post_id, $my_data){
+	if( ! isset($_POST['sunset_contact_email_meta_box_nonce']) ){
+		return;
+	}
+	if( ! wp_verify_nonce($_POST['sunset_contact_email_meta_box_nonce'],
+	 'sunset_save_contact_email_data') ){
+		return;
+	}
+	if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE){
+		return;
+	}
+	if( ! current_user_can('edit_post', $post_id)){
+		return;
+	}
+	if( ! isset($_POST['sunset_contact_email_field']) ){
+		return;
+	}
+	$my_data = sanitize_text_field($_POST['sunset_contact_email_field']);
+	update_post_meta($post_id, '_contact_email_value_key', $my_data);
+}
+// function st_contact_email_callback($post)
+// {
+// 	wp_nonce_field('st_save_contact_email_data','st_contact_email_meta_box_nonce');
+// 	$value = get_post_meta($post->ID,'contact_email_value_key',true);
+// 	echo '<label for = "st_contact_email_field">User Email Address: </lable>';
+// 	echo '<input type = "email" id = "st_contact_email_field" name = "st_contact_email_field" value="'.esc_attr($value).'" size = "25"/>';
+// }
+// function st_contact_save_email_data($post_id)
+// {
+// 	if (!isset($_POST['st_contact_email_meta_box_nonce'])) {
+// 		return;
+// 	}
+// 	if (!wp_verify_nonce($_POST['st_contact_email_meta_box_nonce'],'st_save_contact_email_data')) {
+// 		return;
+// 	}
+// 	if (define('DOING_AUTOSAVE')&& DOING_AUTOSAVE) {
+// 		return;
+// 	}
+// 	if (!current_user_can('edit_post',$post_id)) {
+// 		return;
+// 	}
+// 	if (!isset($_POST['st_contact_email_field'])) {
+// 		return;
+// 	}
+// 	$my_data = sanitize_text_field($_POST['st_contact_email_field']);
+// 	update_post_meta($post_id,'contact_email_value_key',$my_data);
+// }
 
-	function save_your_fields_meta( $post_id ) {
-    	// verify nonce
-    	if ( !wp_verify_nonce( $_POST['your_meta_box_nonce'], basename(__FILE__) ) ) {
-    		return $post_id;
-    	}
-    	// check autosave
-    	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-    		return $post_id;
-    	}
-    	// check permissions
-    	if ( 'page' === $_POST['post_type'] ) {
-    		if ( !current_user_can( 'edit_page', $post_id ) ) {
-    			return $post_id;
-    		} elseif ( !current_user_can( 'edit_post', $post_id ) ) {
-    			return $post_id;
-    		}
-    	}
 
-    	$old = get_post_meta( $post_id, 'your_fields', true );
-    	$new = $_POST['your_fields'];
 
-    	if ( $new && $new !== $old ) {
-    		update_post_meta( $post_id, 'your_fields', $new );
-    	} elseif ( '' === $new && $old ) {
-    		delete_post_meta( $post_id, 'your_fields', $old );
-    	}
-    }
-  
+// function st_add_metabox()
+// {
+// 	add_meta_box('contact_email','User Email',[$this,'st_contact_email_callback'],'room');
 
+// }
+// function st_contact_email_callback($post){
+// 	wp_nonce_field('save_email_data', 'contact_email_meta_box_nonce');
+// 	$value = get_post_meta($post->ID, '_contact_email_value_key', true);
+// 	echo '<label for="contact_email_field">User Email</label>';
+// 	echo '<input type="email" id="contact_email_field" name="contact_email_field" value="'.esc_attr($value).'"/>';
+// }
+// function save_contact_email_data($post_id){
+// 	if(!isset( $_POST['contact_email_meta_box_nonce'])){
+// 		return;
+// 	}
+// 	if(!wp_verify_nonce($_POST['contact_email_meta_box_nonce'],'save_email_data')){
+// 		return;
+// 	}
+// 	if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE){
+// 		return;
+// 	}
+// 	if( ! current_user_can('edit_post', $post_id)){
+// 		return;
+// 	}
+// 	if(!isset($_POST['contact_email_field'])){
+// 		return;
+// 	}
+// 	$my_data = sanitize_text_field($_POST['contact_email_field']);
+// 	update_post_meta($post_id, '_contact_email_value_key', $my_data);
+// }
+
+
+
+
+	
+	// function room_meta_box(){
+	// 	add_meta_box('room-info', 'Thông tin về phòng', [$this,'show_your_fields_meta_box'], 'room');
+	// }
+	// function show_your_fields_meta_box($post){
+	// 	wp_nonce_field('save_contact_email_data', 'contact_email_meta_box_nonce');
+	// 	$value = get_post_meta($post->ID, '_contact_email_value_key', true);
+	// 	echo '<label for="contact_email_field">User email:</label>';
+	// 	echo '<input type="email" id="contact_email_field" name="contact_email_field" value="'. esc_attr($value) . '">';
+	// }
+	// function save_contact_email_data($post_id){
+	// 	if( ! isset($_POST['contact_email_meta_box_nonce']) ){
+	// 		return;
+	// 	}
+	// 	if( ! wp_verify_nonce($_POST['contact_email_meta_box_nonce'], 'show_your_fields_meta_box')){
+	// 		return;
+	// 	}
+	// 	if( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE){
+	// 		return;
+	// 	}
+	// 	if( ! current_user_can('edit_post', $post_id)){
+	// 		return;
+	// 	}
+	// 	if(! isset($_POST['contact_email_field'])){
+	// 		return;
+	// 	}
+	// 	$my_data = sanitize_text_field($_POST['contact_email_field']);
+	// 	update_post_meta($post_id, '_contact_email_value_key', $my_data);
+	// }
+ 
 
 
 
