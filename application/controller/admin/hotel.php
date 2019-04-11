2ee7140cc416424686fc2836481fb61a897c61df
cuongth97 <thocuong97@gmail.com>
2019-04-11 14:35:19 +0700

    cuong

--- application/controller/admin/hotel.php
+++ application/controller/admin/hotel.php
@@ -12,6 +12,12 @@ class ST_Hotel_Admin{
 		add_action('manage_hotel_posts_custom_column',array($this,'hotel_custom_columns'), 10,2);
 		add_action('manage_edit-location_columns',array($this,'location_set_columns'));
 		// add_action('manage_location_columns',array($this,'location_custom_columns'), 10,2);
+		add_action('location_add_form_fields', array ( $this, 'add_location_image' ));
+		add_action('created_location', array($this, 'save_location_image'), 10, 2);
+		add_action('location_edit_form_fields', array ( $this, 'update_location_image' ), 10, 2 );
+		add_action('edited_location', array ( $this, 'updated_location_image' ), 10, 2 );
+		add_action('admin_enqueue_scripts', array( $this, 'load_media' ) );
+		add_action( 'admin_footer', array ( $this, 'add_script' ) );
 	}
 
 	public function customsb_post_type(){
@@ -162,6 +168,106 @@ class ST_Hotel_Admin{
 				break;
 		}
 	}
+
+	public function load_media(){
+			wp_enqueue_media();
+		}
+
+		public function add_location_image () { ?>
+			<div class="form-field term-group">
+				<label for="category-image-id"><?php _e('Image', 'shinetheme'); ?></label>
+				<input type="hidden" id="category-image-id" name="category-image-id" class="custom_media_url" value="">
+				<div id="category-image-wrapper"></div>
+				<p>
+					<input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'shinetheme' ); ?>" />
+					<input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'shinetheme' ); ?>" />
+				</p>
+			</div>
+			<?php
+		}
+
+		public function add_script() { ?>
+			<script>
+				jQuery(document).ready( function($) {
+					function ct_media_upload(button_class) {
+						var _custom_media = true,
+						_orig_send_attachment = wp.media.editor.send.attachment;
+						$('body').on('click', button_class, function(e) {
+							var button_id = '#'+$(this).attr('id');
+							var send_attachment_bkp = wp.media.editor.send.attachment;
+							var button = $(button_id);
+							_custom_media = true;
+							wp.media.editor.send.attachment = function(props, attachment){
+								if ( _custom_media ) {
+									$('#category-image-id').val(attachment.id);
+									$('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+									$('#category-image-wrapper .custom_media_image').attr('src',attachment.url).css('display','block');
+								} else {
+									return _orig_send_attachment.apply( button_id, [props, attachment] );
+								}
+							}
+							wp.media.editor.open(button);
+							return false;
+						});
+					}
+					ct_media_upload('.ct_tax_media_button.button'); 
+					$('body').on('click','.ct_tax_media_remove',function(){
+						$('#category-image-id').val('');
+						$('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+					});
+     // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+     $(document).ajaxComplete(function(event, xhr, settings) {
+     	var queryStringArr = settings.data.split('&');
+     	if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+     		var xml = xhr.responseXML;
+     		$response = $(xml).find('term_id').text();
+     		if($response!=""){
+           // Clear the thumb image
+           $('#category-image-wrapper').html('');
+       }
+   }
+});
+ });
+</script>
+<?php }
+
+public function save_location_image(){
+	if(isset($_POST['category-image-id']) && '' !== $_POST['category-image-id']){
+		$image = $_POST['category-image-id'];
+		add_term_meta($term_id, 'category-image-id', $image, true);
+	}
+}
+public function update_location_image ( $term, $amenities ) { ?>
+	<tr class="form-field term-group-wrap">
+		<th scope="row">
+			<label for="category-image-id"><?php _e( 'Image', 'shinetheme' ); ?></label>
+		</th>
+		<td>
+			<?php $image_id = get_term_meta ( $term -> term_id, 'category-image-id', true ); ?>
+			<input type="hidden" id="category-image-id" name="category-image-id" value="<?php echo $image_id; ?>">
+			<div id="category-image-wrapper">
+				<?php if ( $image_id ) { ?>
+					<?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+				<?php } ?>
+			</div>
+			<p>
+				<input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'shinetheme' ); ?>" />
+				<input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'shinetheme' ); ?>" />
+			</p>
+		</td>
+	</tr>
+	<?php
+}
+public function updated_location_image ( $term_id, $tt_id ) {
+	if( isset( $_POST['category-image-id'] ) && '' !== $_POST['category-image-id'] ){
+		$image = $_POST['category-image-id'];
+		update_term_meta ( $term_id, 'category-image-id', $image );
+	} else {
+		update_term_meta ( $term_id, 'category-image-id', '' );
+	}
+}
+
+
 	//create metabox of hotel
 	 function hotel_meta_box(){
 		add_meta_box('hotel-info','Hotel Information',[$this,'hotel_info_output'],'hotel');
