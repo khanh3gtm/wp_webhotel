379aaf8922c0a6a63e62c1b575142da145772bcc
manhtk <tranducmanhdhbk@gmail.com>
2019-04-19 08:25:27 +0700

    manh

--- application/model/homepage.php
+++ application/model/homepage.php
@@ -7,6 +7,7 @@ class homepage_model extends Model
 	
 	 public function __construct() {
         parent::__construct();
+       
     }
 
      public function __ListHotel(){
@@ -19,10 +20,23 @@ class homepage_model extends Model
      		
      	);
      	$query = new WP_Query( $args );
-          
+
      	return $query;
 
  
+      }
+      public function addColumnForPostTable(){
+        global $wpdb;
+        $version ='1.1.1';
+        $db_version = get_option('st_post_column');
+
+        if($db_version != $version)
+        {
+            $sql =" ALTER TABLE {$wpdb->prefix}posts ADD price varchar(100)   ";        
+            update_option('st_post_column', $version);
+            $wpdb->query($sql);
+        }
+
       }
     public static function inst(){
         static $instane;
